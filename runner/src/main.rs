bba716d984f65780246d6e81339447f8e6a9c115
cryeprecision <muench.rico@gmail.com>
2023-08-10 13:02:22 +0200

    make logging more verbose

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -4,7 +4,12 @@ pub fn init_logger() {
 
     TermLogger::init(
         LevelFilter::Info,
-        ConfigBuilder::default().build(),
+        ConfigBuilder::default()
+            .set_time_format_rfc2822()
+            .set_target_level(LevelFilter::Info)
+            .set_time_offset_to_local()
+            .unwrap()
+            .build(),
         TerminalMode::Mixed,
         ColorChoice::Auto,
     )
