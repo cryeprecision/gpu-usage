bda60f20f1225a6caf1f5e8b6d00e53aab98213f
cryeprecision <muench.rico@gmail.com>
2023-08-10 12:31:11 +0200

    add interval to main and record child stderr

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,7 +1,25 @@
-use gpu_usage;
+pub fn init_logger() {
+    use log::LevelFilter;
+    use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode};
+
+    TermLogger::init(
+        LevelFilter::Info,
+        ConfigBuilder::default().build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )
+    .unwrap();
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let usage = gpu_usage::gpu_usage(5000, "pci:card=1").await.unwrap();
-    println!("{}", serde_json::to_string(&usage).unwrap());
+    let mut ticker = tokio::time::interval(tokio::time::Duration::from_millis(1000));
+    ticker.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
+
+    loop {
+        ticker.tick().await;
+
+        let usage = gpu_usage::gpu_usage("pci:card=1").await.unwrap();
+        log::info!("gpu usage: {}", serde_json::to_string(&usage).unwrap());
+    }
 }
